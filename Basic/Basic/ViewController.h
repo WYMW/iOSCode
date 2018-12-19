24e5b95f7a23bc994720de169b861aa015ddd1b1
WYMW <wymwpr@gmail.com>
2018-12-19 20:08:19 +0800

    add new

---
+++ Basic/Basic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Basic
+//
+//  Created by YmWw on 2018/12/19.
+//  Copyright © 2018 Barry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Basic/BasicTests/BasicTests.m
@@ -0,0 +1,37 @@
+//
+//  BasicTests.m
+//  BasicTests
+//
+//  Created by YmWw on 2018/12/19.
+//  Copyright © 2018 Barry. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BasicTests : XCTestCase
+
+@end
+
+@implementation BasicTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
